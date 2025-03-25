f5d82120823d4d10690ac1f1b418e2946b477440
Niidha <nidhalatheef5965@gmail.com>
2025-03-25 20:34:14 +0530

    vercel.json added

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,6 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
+  build: {
+    outDir: "dist"
+  },
   plugins: [react(), tailwindcss()],
-  
+  server: {
+    port: 3000
+  }
 })
